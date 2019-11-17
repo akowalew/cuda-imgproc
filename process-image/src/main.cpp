8c71d5b996b369665977220e165a4d2d6b840deb
akowalew <ram.techen@gmail.com>
2019-11-17 20:23:09 +0100

    process-image: Add filtering using MeanBlur 5x5

--- process-image/src/main.cpp
+++ process-image/src/main.cpp
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+// main.cpp
+//
+// Contains implementation of entry point to `process-image` application
+//
+// Author: akowalew (ram.techen@gmail.com)
+// Date: 17.11.2019 19:06 CEST
+///////////////////////////////////////////////////////////////////////////////
+
 #include <cstdio>
 
 #include <opencv2/imgproc.hpp>
@@ -6,10 +15,12 @@
 
 #include "imgproc/imgproc.hpp"
 #include "imgproc/format.hpp"
+#include "imgproc/filter.hpp"
 
 // Windows names
 constexpr auto OriginalImage = "Original image";
 constexpr auto HSVImage = "HSV Image";
+constexpr auto BlurredImage = "Blurred Image";
 
 void init()
 {
@@ -29,7 +40,7 @@ void deinit()
     imgproc::deinit();
 }
 
-cv::Mat read_image_bgr(const char* image_path)
+cv::Mat read_bgr_888(const char* image_path)
 {
     assert(image_path != nullptr);
 
@@ -38,11 +49,15 @@ cv::Mat read_image_bgr(const char* image_path)
     auto bgr = cv::imread(image_path);
     cv::imshow(OriginalImage, bgr);
 
+    assert(bgr.type() == CV_8UC3);
+
     return bgr;
 }
 
-cv::Mat convert_bgr_to_hsv(const cv::Mat& bgr)
+cv::Mat bgr_to_hsv_888(const cv::Mat& bgr)
 {
+    assert(bgr.type() == CV_8UC3);
+
     const auto type = bgr.type();
     const auto rows = bgr.rows;
     const auto cols = bgr.cols;
@@ -57,10 +72,40 @@ cv::Mat convert_bgr_to_hsv(const cv::Mat& bgr)
     return hsv;
 }
 
+cv::Mat mean_blur_888(const cv::Mat& src)
+{
+    assert(src.type() == CV_8UC3);
+
+    const auto type = src.type();
+    const auto rows = src.rows;
+    const auto cols = src.cols;
+
+    assert(rows > 0);
+    assert(cols > 0);
+
+    auto blurred = cv::Mat(rows, cols, type);
+
+    const auto kernel_size = 5;
+    float kernel_data[kernel_size*kernel_size] = {
+        1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0,
+        1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0,
+        1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0,
+        1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0,
+        1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0, 1.0/25.0,
+    };
+
+    imgproc::filter2d_888(src.data, blurred.data, cols, rows,
+        kernel_data, kernel_size);
+    cv::imshow(BlurredImage, blurred);
+
+    return blurred;
+}
+
 void process_image(const char* image_path)
 {
-    const auto bgr = read_image_bgr(image_path);
-    const auto hsv = convert_bgr_to_hsv(bgr);
+    const auto bgr = read_bgr_888(image_path);
+    const auto hsv = bgr_to_hsv_888(bgr);
+    const auto blurred = mean_blur_888(hsv);
 
     cv::waitKey(0);
 }
