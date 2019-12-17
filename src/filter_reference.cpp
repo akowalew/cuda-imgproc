29beb252c272a2171c130aae7c8a6e2c4c7fd09c
akowalew <ram.techen@gmail.com>
2019-12-17 22:34:18 +0100

    Rename filter_impl_* -> filter_* and remove plain filter.cpp

--- src/filter_reference.cpp
+++ src/filter_reference.cpp
@@ -1,7 +1,7 @@
 #include "filter.hpp"
 #include <opencv2/imgproc.hpp>
 
-void _filter2d_8(const Image& src, Image& dst, const Image& kernel)
+void filter2d_8(const Image& src, Image& dst, const Image& kernel)
 {
 	const auto ddepth = -1; // Keep depth in destination same as in source
 	cv::filter2D(src, dst, ddepth, kernel);
