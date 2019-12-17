d97b7204b4120e3e8ad6e8bfefa13b473df69e5f
akowalew <ram.techen@gmail.com>
2019-12-17 22:36:10 +0100

    Add Google Benchmark library and add some benchmarks

---
+++ bench/bench_main.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+// bench_main.cpp
+//
+// Entry point for benchmarks of imgproc library
+//
+// Author: akowalew (ram.techen@gmail.com)
+// Date: 17.12.2019 19:32 CEST
+///////////////////////////////////////////////////////////////////////////////
+
+#include <benchmark/benchmark.h>
+
+// Inherit main benchmark routine from Google Benchmark library
+BENCHMARK_MAIN();
---
+++ bench/median_bench.cpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+// median_bench.cpp
+//
+// Contains definitions of benchmarks for median module
+//
+// Author: akowalew (ram.techen@gmail.com)
+// Date: 17.12.2019 19:46 CEST
+///////////////////////////////////////////////////////////////////////////////
+
+#include <benchmark/benchmark.h>
+
+#include "median.hpp"
+
+//! Performs benchmarking of equalize_median_8 function
+static void equalize_median_8(benchmark::State& state, int width, int height, int ksize)
+{
+    // Create source and destination image
+    auto src = Image(width, height, CV_8UC1);
+    auto dst = Image(src.rows, src.cols, CV_8UC1);
+
+    // Perform benchmark
+    for(auto _ : state)
+    {
+        median2d_8(src, dst, ksize);
+    }
+}
+
+BENCHMARK_CAPTURE(equalize_median_8, 640x480x9, 640, 480, 9)
+    ->UseRealTime();
