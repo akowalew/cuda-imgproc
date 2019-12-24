b8aa0e12281da09632ffba19f3aa4893458acb73
Lasica <dobrogo.a@gmail.com>
2019-12-24 17:40:20 +0100

    GPU Cuda version - brutal, global, not tested, yet compiling

--- src/filter_cuda1.cu
+++ src/filter_cuda1.cu
@@ -7,9 +7,28 @@
 
 //Assuming max filter size = 64x64
 
-#define KERNELDIM 6
-#define KERNELSIZE (1<<KERNELDIM) // 64
+#define KERNELDIM 5
+#define KERNELSIZE (1<<KERNELDIM) // 32
+
+#define BLOCKSIZE 32
 __constant__ float CONVKERNEL[KERNELSIZE*KERNELSIZE];
+
+
+__global__ static void filter2d_8_kernel(uchar const* yin, uchar *yout, int n_rows, int n_cols, int K) {
+    int c = threadIdx.x + blockIdx.x*blockDim.x;
+    int r = threadIdx.y + blockIdx.y*blockDim.y;
+    float acc = 0.0f;
+    for(int i=0; i<K; ++i)
+        for(int j=0; j<K; ++j) {
+            acc += static_cast<float>(yin[(r + i)*n_cols + c + j]) * CONVKERNEL[K*i + j];
+        }
+    if(acc > 255.0)
+        acc = 255.0f;
+    if(acc < 0.0)
+        acc = 0.0f;
+    acc += 0.5f;
+    yout[n_cols*r + c] = static_cast<uchar>(acc);
+}
 
 __host__ void filter2d_8(const Image& src, Image& dst, const Image& kernel, int offset)
 {
@@ -30,134 +49,66 @@ __host__ void filter2d_8(const Image& src, Image& dst, const Image& kernel, int 
         throw std::string("Convolution kernel must be a continuous matrix");
     
 //     float const* krnptr = kernel.ptr<float>(0);
-//     uchar * dstptr;
-    uchar const* srcptr;
 //             
-    srcptr = src.ptr<uchar>(0);
     
     // Padding rows and cols size to kernel size granularity
-    int cols = ((src.cols + KERNELSIZE - 1) >> KERNELDIM) << KERNELDIM;
-    int rows = ((src.rows + KERNELSIZE - 1) >> KERNELDIM) << KERNELDIM;
+    // Exact sufficient size is: ((DIM + BLOCK_SIZE-1)/BLOCK_SIZE)*BLOCK_SIZE + KERNEL_SIZE-1
+    int cols = ((src.cols + KERNELSIZE - 1 + BLOCKSIZE - 1) / BLOCKSIZE) * BLOCKSIZE;
+    int rows = ((src.rows + KERNELSIZE - 1 + BLOCKSIZE - 1) / BLOCKSIZE) * BLOCKSIZE;
     
-    float *device_in;
-	float *device_out;
-	float *device_convol;
+    uchar *device_in;
+    uchar *device_out;
+//     float *device_convol;
+
+    checkCudaErrors(cudaSetDevice(0));
+    checkCudaErrors(cudaMalloc(&device_in, sizeof(uchar) * cols * rows));
+    checkCudaErrors(cudaMalloc(&device_out, sizeof(uchar) * cols * rows));
+        
+    uchar const* srcptr;
+    // Copying image data to device with padding
+    for(int r=0; r < src.rows; ++r) {
+        srcptr = src.ptr<uchar>(r);
+        checkCudaErrors(cudaMemcpy(device_in, srcptr, sizeof(uchar)*src.cols, cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemset(device_in + r*cols + src.cols, 0, sizeof(uchar)*(cols-src.cols)));
+    }
+    checkCudaErrors(cudaMemset(device_in + src.rows*cols, 0, sizeof(uchar)*((rows-src.rows)*cols)));
     
-	checkCudaErrors(cudaSetDevice(0));
-	checkCudaErrors(cudaMalloc(&device_in, sizeof(uchar) * cols * rows));
-	checkCudaErrors(cudaMalloc(&device_out, sizeof(float) * cols * rows));
-	//checkCudaErrors(cudaMalloc(&device_convol, sizeof(float)*K*K));
-// 	checkCudaErrors(cudaMemcpy(device_in, yin, dim_bytes, cudaMemcpyHostToDevice));
-// 	//checkCudaErrors(cudaMemcpy(device_out, yout, dim_bytes, cudaMemcpyHostToDevice));
-// 	checkCudaErrors(cudaMemcpy(device_coeff, coeff, sizeof(float)*(n + 1), cudaMemcpyHostToDevice));
-// 	
-// 	unsigned const blocks = (len + K - 1) / K;
-// 
-// 	cudaEvent_t start, stop; // pomiar czasu wykonania jądra 
-// 	checkCudaErrors(cudaEventCreate(&start));
-// 	checkCudaErrors(cudaEventCreate(&stop));
-// 	checkCudaErrors(cudaEventRecord(start, 0));
-// 
-// 	audiofir_kernel<<<blocks, K>>>(device_out, device_in, device_coeff, n, len);
-// 	checkCudaErrors(cudaGetLastError());
-// 	audiofir_kernel<<<blocks, K>>> (device_out+len, device_in+len, device_coeff, n, len);
-// 	checkCudaErrors(cudaGetLastError());
-// 
-// 	checkCudaErrors(cudaEventRecord(stop, 0)); 
-// 	checkCudaErrors(cudaEventSynchronize(stop));
-// 	float elapsedTime; 
-// 	checkCudaErrors(cudaEventElapsedTime(&elapsedTime, start, stop));
-// 	checkCudaErrors(cudaEventDestroy(start));
-// 	checkCudaErrors(cudaEventDestroy(stop));
-// 
-// 	//checkCudaErrors(cudaMemcpy(yin, device_in, dim_bytes, cudaMemcpyDeviceToHost));
-// 	checkCudaErrors(cudaMemcpy(yout, device_out, dim_bytes, cudaMemcpyDeviceToHost));
-// 	cudaFree(device_in);
-// 	cudaFree(device_out);
-// 	cudaFree(device_coeff);
+    // Copying convolution filter coefficients to __constant__ memory
+    checkCudaErrors(cudaMemcpyToSymbol(kernel.ptr<float>(0), CONVKERNEL, (K*K)*sizeof(float), 0, cudaMemcpyHostToDevice));
+    
+    // Zero-ing the device output memory
+    checkCudaErrors(cudaMemset(device_out, 0, sizeof(uchar)*rows*cols));
+    
+    // Initialising cuda Events for calculation time measurement
+    cudaEvent_t start, stop;
+    checkCudaErrors(cudaEventCreate(&start));
+    checkCudaErrors(cudaEventCreate(&stop));
+    checkCudaErrors(cudaEventRecord(start, 0));
+    
+    
+    unsigned const blocks_x = (src.rows - K + 1 + BLOCKSIZE -1) / BLOCKSIZE;
+    unsigned const blocks_y = (src.cols - K + 1 + BLOCKSIZE -1) / BLOCKSIZE;
+    //wywolanie kernela
+    dim3 dimBlock(BLOCKSIZE, BLOCKSIZE), dimGrid(blocks_x, blocks_y);
+    filter2d_8_kernel<<<dimGrid, dimBlock>>>(device_in, device_out, rows, cols, K);
+    checkCudaErrors(cudaGetLastError());
+
+    // Measuring the elapsed time
+    checkCudaErrors(cudaEventRecord(stop, 0)); 
+    checkCudaErrors(cudaEventSynchronize(stop));
+    float elapsedTime; 
+    checkCudaErrors(cudaEventElapsedTime(&elapsedTime, start, stop));
+    checkCudaErrors(cudaEventDestroy(start));
+    checkCudaErrors(cudaEventDestroy(stop));
+
+    
+    uchar * dstptr;
+    for(int i=0; i < src.rows-K+1; ++i) {
+        dstptr = dst.ptr<uchar>(i+offset) + offset;
+        checkCudaErrors(cudaMemcpy(dstptr, device_out + i*cols, sizeof(uchar)*(src.cols - K + 1), cudaMemcpyDeviceToHost));
+    }
+    cudaFree(device_in);
+    cudaFree(device_out);
+    
 // 	printf("GPU (kernel) time = %.3f ms (%6.3f GFLOP/s)\n", elapsedTime, 1e-6 * 2 * ((double)n + 1) * 2 * ((double)len) / elapsedTime);
-    
-
-//     for(int y=0; y < row_limit; ++y) { // Y; iteration of rows
-//         // Destination pointer calculation - setting the right row
-//         dstptr = dst.ptr<uchar>(y + offset) + offset;
-//         
-//         for(int x=0; x < col_limit; ++x) { // X; iteration of columns
-//             // Accumulator for calculations of given round
-//             float acc = 0.0f;
-//             
-//             for(int dy=0; dy < K ; ++dy) // dY
-//                 for(int dx=0; dx < K; ++dx) // dX
-//                     acc += *(krnptr + dy*K + dx) * (*(srcptr + (y + dy)*cols + x + dx));
-//             
-//             // Rounding and trimming the result to uchar value
-//             acc = round(acc);
-//             if(acc < 0.0f)
-//                 acc = 0.0f;
-//             if(acc > 255.0f)
-//                 acc = 255.0f;
-//             
-//             dstptr[x] = static_cast<uchar>(acc);
-//         }
-//     }
-    
-    
 }
-
-__global__ static void filter2d_8_kernel(float const* yin, float *yout, float const *coeff, int n_x, int n_y, int K, int offset) {
-    int i = threadIdx.x + blockIdx.x*blockDim.x;
-}
-
-/*__global__ static void audiofir_kernel(float* yout, float* yin, float* coeff, int n, int len)
-{
-	int i = threadIdx.x + blockIdx.x*blockDim.x; if (i < len) {
-		float acc = 0.0f;
-		for (int k = 0; k <= n; ++k) {
-			if (i >= k) {
-				acc = acc + yin[i - k] * coeff[k];
-			}
-		}
-		yout[i] = acc;
-	}
-}
-void audiofir(
-	float* yout, float* yin, float* coeff, int n, int len, ...)
-{
-	float *device_in;
-	float *device_out;
-	float *device_coeff;
-	unsigned const dim_bytes = sizeof(float) * len * 2;
-	checkCudaErrors(cudaSetDevice(0));
-	checkCudaErrors(cudaMalloc(&device_in, dim_bytes));
-	checkCudaErrors(cudaMalloc(&device_out, dim_bytes));
-	checkCudaErrors(cudaMalloc(&device_coeff, sizeof(float)*(n+1)));
-	checkCudaErrors(cudaMemcpy(device_in, yin, dim_bytes, cudaMemcpyHostToDevice));
-	//checkCudaErrors(cudaMemcpy(device_out, yout, dim_bytes, cudaMemcpyHostToDevice));
-	checkCudaErrors(cudaMemcpy(device_coeff, coeff, sizeof(float)*(n + 1), cudaMemcpyHostToDevice));
-	
-	unsigned const blocks = (len + K - 1) / K;
-
-	cudaEvent_t start, stop; // pomiar czasu wykonania jądra 
-	checkCudaErrors(cudaEventCreate(&start));
-	checkCudaErrors(cudaEventCreate(&stop));
-	checkCudaErrors(cudaEventRecord(start, 0));
-
-	audiofir_kernel<<<blocks, K>>>(device_out, device_in, device_coeff, n, len);
-	checkCudaErrors(cudaGetLastError());
-	audiofir_kernel<<<blocks, K>>> (device_out+len, device_in+len, device_coeff, n, len);
-	checkCudaErrors(cudaGetLastError());
-
-	checkCudaErrors(cudaEventRecord(stop, 0)); 
-	checkCudaErrors(cudaEventSynchronize(stop));
-	float elapsedTime; 
-	checkCudaErrors(cudaEventElapsedTime(&elapsedTime, start, stop));
-	checkCudaErrors(cudaEventDestroy(start));
-	checkCudaErrors(cudaEventDestroy(stop));
-
-	//checkCudaErrors(cudaMemcpy(yin, device_in, dim_bytes, cudaMemcpyDeviceToHost));
-	checkCudaErrors(cudaMemcpy(yout, device_out, dim_bytes, cudaMemcpyDeviceToHost));
-	cudaFree(device_in);
-	cudaFree(device_out);
-	cudaFree(device_coeff);
-	printf("GPU (kernel) time = %.3f ms (%6.3f GFLOP/s)\n", elapsedTime, 1e-6 * 2 * ((double)n + 1) * 2 * ((double)len) / elapsedTime);
-}*/
