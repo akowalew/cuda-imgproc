8d5c797df942264f41410334bf9e0cf1ea5cf94e
akowalew <ram.techen@gmail.com>
2020-01-27 21:36:13 +0100

    cuda_filter: Bring up to work

--- lib/src/cuda/cuda_filter.cu
+++ lib/src/cuda/cuda_filter.cu
@@ -12,119 +12,176 @@
 
 #include "log.hpp"
 
-constexpr size_t KERNELSIZE = 32;
+//
+// Private globals
+//
 
-constexpr size_t K = 32;
+//! Number of threads in block
+static constexpr size_t K = 32;
 
-static __constant__ float CONVKERNEL[KERNELSIZE*KERNELSIZE];
+//! Maximum size of a kernel
+static constexpr size_t KSizeMax = 32;
 
+//! Constant array with filter kernel coefficients
+__constant__ float c_kernel[KSizeMax*KSizeMax];
+
+//! Size of current stored in constant memory kernel
 static size_t ksize;
+
+//
+// Private functions
+//
 
 void cuda_set_filter_kernel(const CudaKernel& kernel)
 {
     ksize = kernel.size;
+
+    LOG_INFO("Setting CUDA kernel for filter %lux%lu\n", ksize, ksize);
+
     const auto buffer = (void*) kernel.data;
     const auto buffer_size = (ksize * ksize * sizeof(CudaKernel::Type));
     const auto buffer_offset = 0;
-    checkCudaErrors(cudaMemcpyToSymbol(CONVKERNEL, buffer, 
-        buffer_size, buffer_offset, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpyToSymbol(c_kernel, buffer, 
+        buffer_size, buffer_offset, cudaMemcpyDeviceToDevice));
 }
 
 __global__ 
 static void filter_kernel(
-    uchar const* yin, uchar *yout, 
-    int n_rows, int n_cols, int ksize)
+    uchar* dst_ex, size_t dpitch_ex,
+    const uchar* src_ex, size_t spitch_ex,  
+    size_t ksize)
 {
-    int c = threadIdx.x + blockIdx.x*blockDim.x;
-    int r = threadIdx.y + blockIdx.y*blockDim.y;
-    float acc = 0.0f;
+    const int x = threadIdx.x + blockIdx.x*blockDim.x;
+    const int y = threadIdx.y + blockIdx.y*blockDim.y;
 
+    float acc = 0.0f;
     for(int i = 0; i < ksize; ++i)
     {
         for(int j = 0; j < ksize; ++j)
         {
-            acc += static_cast<float>(yin[(r + i)*n_cols + c + j]) * CONVKERNEL[ksize*i + j];
+            const auto src_v = static_cast<float>(src_ex[(i+y)*spitch_ex + (j+x)]);
+            const auto kernel_v = c_kernel[i*ksize + j];
+            // printf("%f ", kernel_v);
+            acc += src_v * kernel_v;
         }
     }
 
-    if(acc > 255.0)
+    if(acc > 255.0f)
     {
         acc = 255.0f;
     }
-    if(acc < 0.0)
+    else if(acc < 0.0f)
     {
         acc = 0.0f;
     }
     
     acc += 0.5f;
     
-    yout[n_cols*r + c] = static_cast<uchar>(acc);
+    const auto offset = (ksize / 2);
+    dst_ex[(y+offset)*dpitch_ex + (x+offset)] = static_cast<uchar>(acc);
 }
-
-__host__ void filter(const Image& src, Image& dst, const Image& kernel, int offset)
-{
-    // Setting default offset
-    if(offset < 0)
-    {
-        offset = ksize/2;
-    }
-
-    // Ensure that offset is not higher than filter size
-    assert(offset <= ksize);
-    
-    // Padding rows and cols size to kernel size granularity
-    // Exact sufficient size is: ((DIM + BLOCK_SIZE-1)/BLOCK_SIZE)*BLOCK_SIZE + KERNEL_SIZE-1
-    int cols = ((src.cols + KERNELSIZE - 1 + K - 1) / K) * K;
-    int rows = ((src.rows + KERNELSIZE - 1 + K - 1) / K) * K;
-    
-    uchar *device_in;
-    uchar *device_out;
-
-    checkCudaErrors(cudaMalloc(&device_in, sizeof(uchar) * cols * rows));
-    checkCudaErrors(cudaMalloc(&device_out, sizeof(uchar) * cols * rows));
-        
-    uchar const* srcptr;
-    // Copying image data to device with padding
-    for(int r = 0; r < src.rows; ++r)
-    {
-        srcptr = src.ptr<uchar>(r);
-        checkCudaErrors(cudaMemcpy(device_in, srcptr, sizeof(uchar)*src.cols, cudaMemcpyHostToDevice));
-        checkCudaErrors(cudaMemset(device_in + r*cols + src.cols, 0, sizeof(uchar)*(cols-src.cols)));
-    }
-
-    checkCudaErrors(cudaMemset(device_in + src.rows*cols, 0, sizeof(uchar)*((rows-src.rows)*cols)));
-    
-    // Zero-ing the device output memory
-    checkCudaErrors(cudaMemset(device_out, 0, sizeof(uchar)*rows*cols));
-    
-    unsigned const blocks_x = (src.rows - ksize + 1 + K - 1) / K;
-    unsigned const blocks_y = (src.cols - ksize + 1 + K - 1) / K;
-
-    //wywolanie kernela
-    dim3 dimBlock(K, K);
-    dim3 dimGrid(blocks_x, blocks_y);
-    filter_kernel<<<dimGrid, dimBlock>>>(device_in, device_out, rows, cols, ksize);
-    checkCudaErrors(cudaGetLastError());
-    
-    uchar* dstptr;
-    for(int i = 0; i < src.rows - ksize + 1; ++i) 
-    {
-        dstptr = dst.ptr<uchar>(i+offset) + offset;
-        checkCudaErrors(cudaMemcpy(dstptr, device_out + i*cols, sizeof(uchar)*(src.cols - ksize + 1), cudaMemcpyDeviceToHost));
-    }
-
-    cudaFree(device_in);
-    cudaFree(device_out);
-}
-
-void cuda_filter(CudaImage& dst, const CudaImage& src)
+ 
+void cuda_filter_async(CudaImage& dst, const CudaImage& src)
 {
     // Ensure proper shapes of images
     assert(dst.cols == src.cols);
     assert(dst.rows == src.rows);
+
+    const auto cols = src.cols;
+    const auto rows = src.rows;
+
+    // Setting default offset
+    const auto offset = (ksize / 2);
     
-    // TODO: Launch filtering kernel
-    cuda_image_copy(dst, src);
+    // Padding rows and cols size to kernel size granularity
+    int cols_ex = ((cols + (ksize-1) + (K-1)) / K) * K;
+    int rows_ex = ((rows + (ksize-1) + (K-1)) / K) * K;
+    
+    // Allocate extended images buffers
+    auto src_ex = cuda_create_image(cols_ex, rows_ex);
+    auto dst_ex = cuda_create_image(cols_ex, rows_ex);
+    
+    const auto sdata = (uchar*) src.data;
+    const auto ddata = (uchar*) dst.data;
+    const auto spitch = src.pitch;
+    const auto dpitch = dst.pitch;
+    const auto ddata_ex = (uchar*) dst_ex.data;
+    const auto sdata_ex = (uchar*) src_ex.data;
+    const auto dpitch_ex = dst_ex.pitch;
+    const auto spitch_ex = src_ex.pitch;
+
+    // Copy source buffer into extended source buffer
+    checkCudaErrors(cudaMemcpy2D(
+        sdata_ex + offset*spitch_ex + offset*sizeof(uchar), spitch_ex, 
+        sdata, spitch, 
+        cols*sizeof(uchar), rows, 
+        cudaMemcpyDeviceToDevice));
+
+    // Set unused pixels of extended source buffer to zeros (top bar)
+    checkCudaErrors(cudaMemset2D(
+        sdata_ex, spitch_ex, 0,
+        cols_ex*sizeof(uchar), offset));
+
+    // Set unused pixels of extended source buffer to zeros (Left bar)
+    checkCudaErrors(cudaMemset2D(
+        sdata_ex + offset*spitch_ex, spitch_ex, 0,
+        offset*sizeof(uchar), rows));
+
+    // Set unused pixels of extended source buffer to zeros (Right bar)
+    checkCudaErrors(cudaMemset2D(
+        sdata_ex + offset*spitch_ex + (offset+cols)*sizeof(uchar), spitch_ex, 0,
+        (cols_ex-cols-offset)*sizeof(uchar), rows));
+
+    // Set unused pixels of extended source buffer to zeros (bottom bar)
+    checkCudaErrors(cudaMemset2D(
+        sdata_ex + (offset+rows)*spitch_ex, spitch_ex, 0,
+        cols_ex*sizeof(uchar), (rows_ex-rows-offset)));
+
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    // Dimensions of each block (in threads)
+    const unsigned dim_block_x = K;
+    const unsigned dim_block_y = K;
+    const auto dim_block = dim3(dim_block_x, dim_block_y);
+
+    // Dimensions of a grid (in blocks)
+    const unsigned dim_grid_x = ((cols+K-1) / K);
+    const unsigned dim_grid_y = ((rows+K-1) / K);
+    const auto dim_grid = dim3(dim_grid_x, dim_grid_y);
+
+    LOG_INFO("dim_grid %lux%lu ex %lux%lu norm %lux%lu\n", 
+        dim_grid_x, dim_grid_y,
+        cols_ex, rows_ex,
+        cols, rows);
+
+    // Launch filtering kernel
+    filter_kernel<<<dim_grid, dim_block>>>(
+        ddata_ex, dpitch_ex, 
+        sdata_ex, spitch_ex, 
+        ksize);
+
+    // Check for kernel launch errors
+    checkCudaErrors(cudaGetLastError());
+    
+    // Copy results to destination buffer
+    checkCudaErrors(cudaMemcpy2D(
+        ddata, dpitch, 
+        ddata_ex + offset*dpitch_ex + offset*sizeof(uchar), dpitch_ex, 
+        cols*sizeof(uchar), rows, 
+        cudaMemcpyDeviceToDevice));
+
+    // Free temporary images
+    cuda_free_image(dst_ex);
+    cuda_free_image(src_ex);
+}
+
+void cuda_filter(CudaImage& dst, const CudaImage& src)
+{
+    // Launch convolution filtering asynchronously
+    cuda_filter_async(dst, src);
+
+    // Wait for device to finish
+    checkCudaErrors(cudaDeviceSynchronize());
 }
 
 void cuda_filter(CudaImage& dst, const CudaImage& src, const CudaKernel& kernel)
