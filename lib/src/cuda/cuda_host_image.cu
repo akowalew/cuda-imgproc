910a91f6de3c6b415474a380ff5857a1d70566a3
akowalew <ram.techen@gmail.com>
2020-01-31 01:45:04 +0100

    cuda_host_image: Add more verbose logs

--- lib/src/cuda/cuda_host_image.cu
+++ lib/src/cuda/cuda_host_image.cu
@@ -10,6 +10,8 @@
 
 #include <helper_cuda.h>
 
+#include "log.hpp"
+
 CudaHostImage cuda_create_host_image(size_t cols, size_t rows)
 {
 	return create_image(cols, rows);
@@ -24,8 +26,11 @@ void cuda_host_image_register(const CudaHostImage& h_img)
 {
 	const auto cols = h_img.cols;
 	const auto rows = h_img.rows;
-	const auto size = (cols * rows * sizeof(uchar));
 	const auto data = h_img.data;
+
+	LOG_INFO("Registering host image %lux%lu at 0x%p\n", cols, rows, data);
+
+	const auto size = (cols * rows * sizeof(uchar));
 	const auto flags = cudaHostRegisterDefault;
 	checkCudaErrors(cudaHostRegister(data, size, flags));
 }
@@ -33,5 +38,8 @@ void cuda_host_image_register(const CudaHostImage& h_img)
 void cuda_host_image_unregister(const CudaHostImage& h_img)
 {
 	const auto data = h_img.data;
+	
+	LOG_INFO("Unregistering host image 0x%p\n", data);
+	
 	checkCudaErrors(cudaHostUnregister(data));
 }
